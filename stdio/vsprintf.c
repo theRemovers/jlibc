82dc39c756b5e238c5e433b24726ee6e46d13f6c
Seb/The Removers <SebRmv@jagware.org>
2008-01-15 11:41:22 +0000

    git-svn-id: svn://obiwan/Jaguar@52 1d14b586-ad98-11dc-b037-dddde97c3f5c

---
+++ stdio/vsprintf.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+static int putc(FILE *stream, char c) {
+  *((char*)stream->data++) = c;
+  return (int)c;
+}
+
+static int puts(FILE *stream, const char *s) {
+  int n = strlen(s);
+  bcopy(s,(char*)stream->data,n);
+  stream->data = (char *)stream->data + n;
+  return n;
+}
+
+static size_t write(FILE *stream, const  void  *ptr,  size_t  size,  size_t  nmemb) {
+  return EOF;
+}
+
+static int eof(FILE *stream) {
+  return 0;
+}
+
+int vsprintf(char *str,const char *fmt, va_list ap) {
+  if(str == NULL) return -1;
+  FILE *fp = malloc(sizeof(FILE));
+  fp->data = str;
+  fp->eof = eof;
+  fp->putc = putc;
+  fp->puts = puts;
+  fp->write = write;
+  fp->flush = NULL;
+  fp->close = NULL;
+  int nb = vfprintf(fp,fmt,ap);
+  free(fp);
+  *(str + nb) = 0;
+  return nb;
+}
