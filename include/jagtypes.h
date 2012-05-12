385f835a4857536a3dc894f52fd79e56ec86af07
Seb/The Removers <SebRmv@jagware.org>
2012-05-12 12:01:33 +0200

    defining signed types

--- include/jagtypes.h
+++ include/jagtypes.h
@@ -22,9 +22,13 @@
 #ifndef __JAGTYPES_H
 #define __JAGTYPES_H
 
+typedef char int8_t;
 typedef unsigned char uint8_t;
+typedef short int int16_t;
 typedef unsigned short int uint16_t;
+typedef long int int32_t;
 typedef unsigned long int uint32_t;
+typedef long long int int64_t;
 typedef unsigned long long int uint64_t;
 
 /** A phrase is a 64 bit integer */
